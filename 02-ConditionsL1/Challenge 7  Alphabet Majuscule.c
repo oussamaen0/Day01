398691503d402b8b70eb07b3554804f0af0c30ca
oussama en-naji <artouss2@gmail.com>
2024-09-25 14:35:35 +0200

    correct

--- 02-ConditionsL1/Challenge 7  Alphabet Majuscule.c
+++ 02-ConditionsL1/Challenge 7  Alphabet Majuscule.c
@@ -5,7 +5,7 @@ int main()
 char x;
 printf("give me alphabet i will tell you if alphabet is uppercase and lowercase : ");
 scanf("%c", &x);
-if (65 >= x && x <= 90)
+if (65 <= x && x <= 90)
 {printf("alphabet is uppercase");}
 else
 {printf("alphabet is lowercase");}
