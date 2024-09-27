ac030dd62db065b2cd49168e29b686d075b8e58b
oussama en-naji <artouss2@gmail.com>
2024-09-27 11:51:29 +0200

    new ex

---
+++ 05-Tableaux/Challenge 2 Saisie et Affichage des Éléments.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int main() 
+{
+    int size, number;
+        printf("entre la taille de tablaux : ");
+        scanf("%d", &size);
+        int tab[size];
+            for(int i = 0; i < size; i++)
+                {
+                    printf("entre you number : ");
+                    scanf("%d", &number);
+                        tab[i] = number;
+                }
+                    for(int i = 0; i < size; i++)
+                     {
+                        printf("%d , ", tab[i]);
+                    }
+    return 0;
+}
