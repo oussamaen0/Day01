088f7a2a3d7eab30c95722a18db8423c919990b8
oussamaen0 <artouss2@gmail.com>
2024-09-29 00:21:56 +0100

    correct

---
+++ 05-Tableaux/Challenge 9  Inversion d'un Tableau.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int size,mult;
+        printf("entre size de tablaux : ");
+        scanf("%d",&size);
+    int tab[size];
+    int N;
+        for(int i = 0; i < size;i++){
+            printf("entre les nombre : ");
+            scanf("%d",&N);
+            tab[i] = N;
+        }
+       for(int i = size-1; i >= 0;i--){
+        printf("%d\n",tab[i]);}
+        
+        return 0;
+}
