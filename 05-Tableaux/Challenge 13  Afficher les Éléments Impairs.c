8988902c361e1623882728c9bfa85e8e735b0831
oussama en-naji <artouss2@gmail.com>
2024-09-29 13:38:54 +0200

    nex ex

---
+++ 05-Tableaux/Challenge 13  Afficher les Éléments Impairs.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int taille,i;
+
+    printf("entrer la taille:");
+    scanf("%d",&taille);
+
+    int tab[taille];
+
+    for(i=0;i<taille;i++){
+
+        printf("entre les valeurs : ");
+        scanf("%d",&tab[i]);
+     }
+
+     for(i=0;i<taille;i++){
+        if(tab[i]%2!=0)
+            {
+            printf(" %d impair\n",tab[i]);
+
+            }
+}
+
+return 0;
+}
