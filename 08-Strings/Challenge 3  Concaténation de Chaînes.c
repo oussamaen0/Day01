2d68bd9c115dcc2766ca931c7b3cea97a9e84125
oussama en-naji <artouss2@gmail.com>
2024-10-01 12:10:55 +0200

    new ex

---
+++ 08-Strings/Challenge 3  Concaténation de Chaînes.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char c[100];
+    char d[100];
+        printf("entre your chaine caracter : ");
+        gets(c);
+         printf("entre your chaine caracter : ");
+        gets(d);
+        printf("vous avez entre : %s %s", c,d);
+
+    return 0;
+}
