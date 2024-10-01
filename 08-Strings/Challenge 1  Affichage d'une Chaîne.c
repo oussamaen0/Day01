934a2ff095ce8ff474977175c0c83d50ad403b57
oussama en-naji <artouss2@gmail.com>
2024-10-01 11:27:04 +0200

    new ex

---
+++ 08-Strings/Challenge 1  Affichage d'une Chaîne.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char c[100];
+        printf("entre your chaine caracter : ");
+        gets(c);
+        printf("vous avez entre : %s", c);
+
+    return 0;
+}
